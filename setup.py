b4d56083571315bcd2d9b62c13c76a904b79079e
Mike Dingjan <m.dingjan@labdigital.nl>
2022-06-12 14:59:20 +0200

    Update versions to test against

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    "wagtail>=2.11,<3",
+    "wagtail>=2.16,<3.1",
 ]
 
 testing_extras = ["coverage>=3.7.0"]
@@ -30,10 +30,11 @@ setup(
     extras_require={"testing": testing_extras},
     classifiers=[
         "Framework :: Django",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "Framework :: Wagtail",
         "Framework :: Wagtail :: 2",
+        "Framework :: Wagtail :: 3",
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         "License :: Public Domain",
         "Programming Language :: Python",
